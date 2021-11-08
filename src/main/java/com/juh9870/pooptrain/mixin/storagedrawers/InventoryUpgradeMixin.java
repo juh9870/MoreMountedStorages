e803e1e7efa5dc82c5542ad01f9b8b54945c1276
juh9870 <juh9870@ukr.net>
2021-11-09 01:11:46 +0200

    Fixed inventory validity check for Storage Drawers upgrade interface

---
+++ src/main/java/com/juh9870/pooptrain/mixin/storagedrawers/InventoryUpgradeMixin.java
@@ -0,0 +1,23 @@
+package com.juh9870.pooptrain.mixin.storagedrawers;
+
+import com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawers;
+import com.jaquadro.minecraft.storagedrawers.inventory.InventoryUpgrade;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(InventoryUpgrade.class)
+public class InventoryUpgradeMixin {
+	@Shadow(remap = false)
+	private TileEntityDrawers tile;
+
+	@Inject(at = @At("HEAD"), method = "stillValid(Lnet/minecraft/entity/player/PlayerEntity;)Z", remap = false, cancellable = true)
+	public void pooptrain_stillValid(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+		if (tile.getLevel().getBlockEntity(tile.getBlockPos()) != tile ||
+				player.distanceToSqr((double) tile.getBlockPos().getX() + 0.5D, (double) tile.getBlockPos().getY() + 0.5D, (double) tile.getBlockPos().getZ() + 0.5D) > 64.0D)
+			cir.setReturnValue(false);
+	}
+}
