01b841fc4e62a8d3fc0b108fdb07b16b4d36b2c4
juh9870 <juh9870@ukr.net>
2023-02-15 12:27:34 +0200

    fix: updated TrashCans support
    Fixes #11

--- src/main/java/com/juh9870/moremountedstorages/helpers/TrashCanHandler.java
+++ src/main/java/com/juh9870/moremountedstorages/helpers/TrashCanHandler.java
@@ -1,41 +1,38 @@
 package com.juh9870.moremountedstorages.helpers;
 
-import com.juh9870.moremountedstorages.ContraptionItemStackHandler;
-import com.juh9870.moremountedstorages.ContraptionStorageRegistry;
-import com.juh9870.moremountedstorages.integrations.trashcans.TrashCansRegistry;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
 
 public abstract class TrashCanHandler extends SmartItemStackHandler {
 
-	@Override
-	public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
-	}
+    @Override
+    public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
+    }
 
-	@Nonnull
-	@Override
-	public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-		if (!isItemValid(slot, stack)) return stack;
-		return ItemStack.EMPTY;
-	}
+    @Nonnull
+    @Override
+    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+        if (!isItemValid(slot, stack)) return stack;
+        return ItemStack.EMPTY;
+    }
 
-	@Nonnull
-	public ItemStack extractItem(int slot, int amount, boolean simulate) {
-		return ItemStack.EMPTY;
-	}
+    @Nonnull
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        return ItemStack.EMPTY;
+    }
 
-	public int getSlotLimit(int slot) {
-		return 2147483647;
-	}
+    public int getSlotLimit(int slot) {
+        return 2147483647;
+    }
 
-	@Nonnull
-	public ItemStack getStackInSlot(int slot) {
-		return ItemStack.EMPTY;
-	}
+    @Nonnull
+    public ItemStack getStackInSlot(int slot) {
+        return ItemStack.EMPTY;
+    }
 
-	@Override
-	public int getPriority() {
-		return PRIORITY_TRASH;
-	}
+    @Override
+    public int getPriority() {
+        return PRIORITY_TRASH;
+    }
 }
